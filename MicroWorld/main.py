093da08acc59763f3cc517c2a72eed740dedc193
Gabriela <gabrielan80@gmail.com>
2024-07-07 13:52:03 +0200

    7.07
    vol 2

--- MicroWorld/main.py
+++ MicroWorld/main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import Stokes_2D
-r01 = np.array([-2, 2.0001]) #defining positions of the force
-F = np.array([1, 1])
-r02 = np.array([1.5, 2.0001])
-r03 = np.array([3, 2.0001])
-singularity1 = Stokes_2D.Equations2D(-4, 4, 0.01, "wall") #creating an object
-singularity1.totality_par(r01, -F, 3)
-singularity1.totality_par(r02, F, 3)
-singularity1.totality_par(r03, -F, 3)
-singularity1.plot(title="example2_par_wall.png")
+r01 = np.array([0, 0.5001]) #defining positions of the force
+F1 = np.array([1, 1]) #defining forces values
+r02 = np.array([3.0001, 3.0001]) #defining positions of the force
+F2 = np.array([2, 2]) #defining forces values
+r03 = np.array([-3, 2.0001]) #defining positions of the force
+F3 = np.array([0.5, 3])
+singularity1 = Stokes_2D.Equations2D(-4, 4, 0.13, "free") #creating an object
+singularity1.totality_per(r01, F1, 3) #ok
+
+singularity1.plot() #plotting and saving
 singularity1.show() #showing plot
