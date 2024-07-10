0b5b7f039816c96269896a711e0d4e3feb0418eb
Gabriela <gabrielan80@gmail.com>
2024-07-10 10:03:40 +0200

    Plot streamlines corrected colorbar

--- MicroWorld/main.py
+++ MicroWorld/main.py
@@ -1,13 +1,11 @@
 import numpy as np
 import Stokes_2D
-r01 = np.array([0, 0.5001]) #defining positions of the force
-F1 = np.array([1, 1]) #defining forces values
-r02 = np.array([3.0001, 3.0001]) #defining positions of the force
-F2 = np.array([2, 2]) #defining forces values
-r03 = np.array([-3, 2.0001]) #defining positions of the force
-F3 = np.array([0.5, 3])
+r0 = np.array([0, 0.0001]) #defining position of the force
+F = np.array([0, 1]) #defining force values
 singularity1 = Stokes_2D.Equations2D(-4, 4, 0.01, "free") #creating an object
-singularity1.totality_per(r01, F1, 3) #ok
-
-singularity1.plot() #plotting and saving
+xstart = np.linspace(-3.9, 3.9, 21)
+ystart = np.linspace(-3.9, -3.9, 21)
+singularity1.stokeslet(r0, F) #calling stokeslet
+singularity1.streamlines(xstart, ystart, mesh=True, direction=True,
+title="Stokeslet_streamlines.png") #plotting and saving
 singularity1.show() #showing plot
